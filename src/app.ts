df601c74a21574732bafd1f27d99bf091a06b9c0
bbearcookie <aodem@naver.com>
2023-10-11 17:31:58 +0900

    chore: DB datasource 연결

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import app from '@/configs/express';
+import { AppDataSource } from '@/configs/dataSource';
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+AppDataSource.initialize()
+  .then(() => console.log('Database connected!'))
+  .catch((err) => console.log(err));
 
 app.listen(5010, () => {
   console.log('Server running on port 5010');
